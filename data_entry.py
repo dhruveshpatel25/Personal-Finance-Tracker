20ee4d72b5e27234186165f52b468b6d553fa6d6
Dhruvesh Jayesh Patel <dhruveshpatel-cmpn@atharvacoe.ac.in>
2025-03-09 19:52:31 +0530

    second commit

--- data_entry.py
+++ data_entry.py
@@ -1,38 +1,80 @@
 from datetime import datetime
 
 date_format="%d-%m-%Y"
-CATEGORIES={"I":"Income","E":"Expense"}
 
+CATEGORIES={
+    "I":"Income",
+    "E":"Expense"
+}
+
+#get the date from the user
 def get_date(prompt,allow_default=False):
+
+    #take the input from user for date
     date_str=input(prompt)
+
+    #if user presses enter and allow_default is True, return today's date
     if allow_default and not date_str:
+
+        #return today's date in the specified format
         return datetime.now().strftime(date_format)
     
     try:
+
+        #convert the string into date time object
         valid_date=datetime.strptime(date_str,date_format)
+
+        #return the date in the specified format
         return valid_date.strftime(date_format)
     except ValueError:
-        print("Invalid date format. Please use DD-MM-YYYY")
-        return get_date(prompt,allow_default)
 
+        #if the date is not in the specified format, catch the ValueError and print the error message
+        print("Invalid date format. Please use DD-MM-YYYY")
+
+        #and call the function recursively
+        return get_date(prompt,allow_default)
+    
+#get the amount from the user
 def get_amount():
     try:
+
+        #take the input from the user for amount
         amount=float(input("Enter the amount: "))
+
+        #check if the amount is greater than 0
         if amount<=0:
+
+            #if less raise error
             raise ValueError("Amount must be greater than 0")
+        
+        #if greater return the amonunt
         return amount
     except ValueError as e:
-        print(e)
-        return get_amount()
 
+        #if the amount is not a valid number, catch the ValueError and print the error message
+        print(e)
+
+        #and call the function recursively
+        return get_amount()
+    
+#get category from the user
 def get_category():
+
+    #take the input from the user for category
     category=input("Enter the category('I' for Income or 'E' for Expense): ").upper()
+
+    #check if the category is in the CATEGORIES dictionary
     if category in CATEGORIES:
         return CATEGORIES[category]
     
+    #if not, print the error message
     print("Invalid category. Please enter 'I' or 'E'")
+
+    #and call the function recursively
     return get_category()
 
+#get description from the user
 def get_description():
-    return input("Enter a description: ")
 
+    #take the input from the user for description
+    return input("Enter a description: ")
--- main.py
+++ main.py
@@ -9,15 +9,28 @@ class CSV:
     COLUMNS=['date','amount','category','description']
     DATE_FORMAT="%d-%m-%Y"
 
+    #classmethod used so we can modify class-level variables
     @classmethod
+
+    #initialize the csv file
     def initialize_csv(cls):
         try:
+
+            #try to read the csv file
             pd.read_csv(cls.CSV_FILE)
+
+        #if the file is not found 
         except FileNotFoundError:
+
+            #create a new csv file with the specified columns
             df=pd.DataFrame(columns=cls.COLUMNS)
+
+            #save the dataframe to the csv file
             df.to_csv(cls.CSV_FILE,index=False)
 
     @classmethod
+
+    #add entry
     def add_entry(cls,date,amount,category,description):
         new_entry={
             "date":date,
@@ -25,75 +38,152 @@ class CSV:
             "category":category,
             "description":description
         }
+
+        #open the csv file in append mode
         with open(cls.CSV_FILE,"a",newline='') as csvfile:
+
+            #write the new entry to the csv file
+            #DictWriter is used to write the dictionary to the csv file
             write=csv.DictWriter(csvfile,fieldnames=cls.COLUMNS)
+
+            #writerow is used to write the row to the csv file
             write.writerow(new_entry)
+
+            #after writing the entry, print the message
             print("Entry added successfully")
 
     @classmethod
+
+    #get transactions
     def get_transactions(cls,start_date,end_date):
+
+        #read the csv file
         df=pd.read_csv(cls.CSV_FILE)
+
+        #convert the date column to datetime object and over write in date column
         df['date']=pd.to_datetime(df['date'],format=CSV.DATE_FORMAT)
+
+        #convert the start date to date time object from string
         start_date=datetime.strptime(start_date,cls.DATE_FORMAT)
+
+        #convert the end date to date time object from string
         end_date=datetime.strptime(end_date,cls.DATE_FORMAT)
+
+        #check if the current date in csv file is between the start date and end date
         mask=(df['date']>=start_date)&(df['date']<=end_date)
+
+        #locate all the rows which satisfied the previous condition
         filtered_df=df.loc[mask]
 
+        #check if there exist ant data in range
         if filtered_df.empty:
+
+            #if no transactions are found, print the message
             print("No transactions found")
         else:
+
+            #transaction range dates
             print(f"Transactions between {start_date.strftime(CSV.DATE_FORMAT)} and {end_date.strftime(CSV.DATE_FORMAT)}")
+
+            #print all transactions
+            #convert to string,no index to be printed,format each date to the specified format
             print(filtered_df.to_string(index=False,formatters={"date":lambda x:x.strftime(CSV.DATE_FORMAT)}))
+
+            #calculate the total income 
             total_income=filtered_df[filtered_df['category']=="Income"]['amount'].sum()
+
+            #calculate the total expense
             total_expense=filtered_df[filtered_df['category']=='Expense']['amount'].sum()
 
+            #print the summary
             print("\nSummary: ")
             print(f"Total Income: {total_income:.2f}")
             print(f"Total Expense: {total_expense:.2f}")
             print(f"Savings: {total_income-total_expense:.2f}")
-            
+
+        #return the filtered date 
         return filtered_df
 
-
+#add new transaction
 def add():
+
+    #initialize the csv file
     CSV.initialize_csv()
+
+    #get the date, amount, category and description from the user
     date=get_date("Enter the date (DD-MM-YYYY) or ENTER for today's date: ",allow_default=True)
     amount=get_amount()
     category=get_category()
     description=get_description()
 
+    #add the entry to the csv file
     CSV.add_entry(date,amount,category,description)
 
+#plot the transactions
 def plot_transactions(df):
+
+    #set the date as index
     df.set_index('date',inplace=True)
+
+    #resample the data by day
+    #sum clculates the total amount for each day
+    #reindex fills the missing dates with 0
     income_df=df[df['category']=='Income'].resample('D').sum().reindex(df.index,fill_value=0)
     expense_df=df[df['category']=='Expense'].resample('D').sum().reindex(df.index,fill_value=0)
 
+    #width,height
     plt.figure(figsize=(10,5))
+
+    #(x-axis,y-axis,label,color)
     plt.plot(income_df.index,income_df['amount'],label='Income',color='green')
     plt.plot(expense_df.index,expense_df['amount'],label='Expense',color='red')
+
+    #x-axis label
     plt.xlabel('Date')
+
+    #y-axis label
     plt.ylabel('Amount')
+
+    #title of the plot
     plt.title('Income and Expense Over Time')
+
+    #show legend in plot
     plt.legend()
+
+    #show grid in plot
     plt.grid(True)
+
+    #show the plot
     plt.show()
 
 def main():
     while True:
+
+        #print the menu
         print("\n1. Add new transaction")
         print("2. View transactions")
         print("3. Exit")
         choice=input("Enter your choice: ")
 
+        #check the choice
         if choice=="1":
+
+            #call the add function
             add()
         elif choice=="2":
+
+            #get the start date and end date from the user
             start_date=get_date("Enter the start date (DD-MM-YYYY): ")
             end_date=get_date("Enter the end date (DD-MM-YYYY): ")
+
+            #get the transactions between the start date and end date
             df=CSV.get_transactions(start_date,end_date)
+
+            #if the user wants to plot the transactions
             if input("Do you want to plot the transactions? (y/n): ").lower()=="y":
                 plot_transactions(df)
+
+        #if the user wants to exit
         elif choice=="3":
             print("Exiting...")
             break
